85788532c7e74666db4e31d4c43c57f1f643f2f4
Alzwimn <alzwimn@gmail.com>
2020-12-01 17:27:42 -0300

    add simple tests

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,9 +1,26 @@
-import { Utils } from "../app/Utils";
+import { Utils } from '../app/Utils';
 
 describe ('Utils test suite', () => {
 
   test('First test', () => {
-    const testString = Utils.toUpperCase('testing string')
-    console.log(testString);
+    const result = Utils.toUpperCase('testing string')
+    expect(result).toBe('TESTING STRING')
+  })
+
+  test('Parse simple URL', () => {
+    const parsedUrl = Utils.parseUrl('http://localhost:3000/login')
+    expect(parsedUrl.href).toBe('http://localhost:3000/login')
+    expect(parsedUrl.port).toBe('3000')
+    expect(parsedUrl.protocol).toBe('http:')
+    expect(parsedUrl.query).toEqual({})
+  })
+
+  test('parse URL with query', () => {
+    const parsedUrl = Utils.parseUrl('http://localhost:3000/login?user=user&password=pass')
+    const expectedQuery = {
+      user: 'user',
+      password: 'pass'
+    }
+    expect(parsedUrl.query).toEqual(expectedQuery)
   })
 })
